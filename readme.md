ecf7a31648cf9f10d1539e96d6bf752fb1bd89b6
seanedw1 <sedwards@fullsail.edu>
2016-10-18 15:09:23 -0400

    refactor for best practise

--- readme.md
+++ readme.md
@@ -1,17 +1,13 @@
 # Documentation for NPM package utilitytoolwash v1.0.2
 
-
-
  [ ![Codeship Status for URL Shortener](https://codeship.com/projects/59b912f0-76d2-0134-9160-32ede8a13401/status?branch=master)](https://codeship.com/projects/179701)
 
-<br>
-# install
+
+# Install
 Write the below code in the terminal for your application.
 ```
 npm i -s utilitytoolwash
 ```
-
-
 
 ### How to Use the Package
 
--- src/debug.js
+++ src/debug.js
@@ -3,15 +3,10 @@ const fs = require('fs');
 const colors = require('colors');
 
 // Debug Module, if DEBUG=true nodemon src/server.js console.log will be ON
-module.exports = function debugWash(date, msg, obj) {
+exports.debugWash = (date, msg, obj) => {
   if (process.env.DEBUG) {
     // Console logging data
     console.log(date.inverse, msg.bgYellow, obj);
-
-
-// Sending Console.log Messages to a Logs/debugs.log file
-    JSON.stringify(obj);
-    fs.appendFile('./logs/debugs.log', `${date} ${msg} ${JSON.stringify(obj)} \n`
   );
   }
 };
