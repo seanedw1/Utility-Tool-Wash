9a55031da80db1ba130d27ec81108882dd7d1579
seanedw1 <sedwards@fullsail.edu>
2016-10-20 19:53:46 -0400

    its alive -- version bumper

--- src/debug.js
+++ src/debug.js
@@ -18,3 +18,38 @@ exports.debugWash = (title, obj, status) => {
     }
   }
 };
+
+
+// bump method takes two params version to be bumped and type of update
+exports.bump = (version, updateType) => {
+  // split([separator[, limit]]) method splits a String object into an array of strings
+  const spliter = version.split('.');
+
+  // parseInt(string, radix); - parses a string argument and returns an integer
+  const major = parseInt(spliter[0], 10);
+  const minor = parseInt(spliter[1], 10);
+  const patch = parseInt(spliter[2], 10);
+
+  // switch statement for kind of version bump
+  switch (updateType) {
+    // if major changes
+    case major:
+      spliter[0]++;
+      spliter[1] = 0;
+      spliter[2] = 0;
+      console.log(spliter[0] + '.' + spliter[1] + '.' + spliter[2]);
+      break;
+    // if minor changes
+    case minor:
+      spliter[1]++;
+      spliter[2] = 0;
+      console.log(spliter[0] + '.' + spliter[1] + '.' + spliter[2]);
+      break;
+    // if hotfix
+    case patch:
+      spliter[2]++;
+      console.log(spliter[0] + '.' + spliter[1] + '.' + spliter[2]);
+      break;
+    default: break;
+  }
+};
